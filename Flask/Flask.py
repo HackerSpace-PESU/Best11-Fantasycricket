a674d1c51a1c75c666fcb698748b145caca2e8a8
royston <tauro.royston@gmail.com>
2020-06-22 11:54:16 +0530

    updated Flask
    Former-commit-id: 062fa9b64ecbbf6350c6e2b89d3274ecf82e1faf [formerly 6bfc352e5c47104b734dba5690b0c2f639660662]
    Former-commit-id: 430aecd9b8e7303f07feffe68fd595f6fec4e7d2

--- Flask/Flask.py
+++ Flask/Flask.py
@@ -12,18 +12,17 @@ app.secret_key = "lol"
 @app.route("/", methods=['GET', 'POST'])
 def home():
      if request.method == "POST":
-         session['match'] = request.form['match']
-         
+         session['match'] = request.form['match']       
          return redirect(url_for("result"))
      else:
          return render_template("index.html")
      
 @app.route("/results", methods=['GET', 'POST'])
 def result():
-    team1 = session['match'].split()[0]
-    team2 = session['match'].split()[2]
+    #team1 = session['match'].split()[0]
+    #team2 = session['match'].split()[2]
     
-    return render_template("result.html", t1 = team1, t2 = team2)
+    return render_template("result.html", t1 = session["match"])
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
