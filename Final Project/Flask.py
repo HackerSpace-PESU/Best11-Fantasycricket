3a702f473f51fba4d2f66fc7309c578473289783
royston <tauro.royston@gmail.com>
2020-06-25 18:09:31 +0530

    Final

--- Final Project/Flask.py
+++ Final Project/Flask.py
@@ -41,10 +41,23 @@ def result():
             players.append("Liam Plunkett"+x)
         else:
             players.append(i[:i.find('\xa0')]+x)
-    
-    return render_template("result.html", t1=players[0], t2=players[1], t3=players[2]
-                           , t4=players[3], t5=players[4], t6=players[5], t7=players[6]
-                           , t8=players[7], t9=players[8], t10=players[9], t11=players[10])
+    c=[0,0,0,0,0,0,0,0,0,0,0]
+    v=[0,0,0,0,0,0,0,0,0,0,0]
+    for i in range(len(players)):
+        if '(C)' in players[i]:
+            c[i]="(C)"
+            v[i]=""
+            players[i]=players[i][:-3]
+        elif '(VC)' in players[i]:
+            v[i] ="(VC)"
+            c[i]=""
+            players[i]=players[i][:-4]
+        else:
+            v[i]=""
+            c[i]=""
+    return render_template("result.html", c1=c[0],v1=v[0],t1=players[0], c2=c[1],v2=v[1],t2=players[1], c3=c[2],v3=v[2],t3=players[2]
+                           ,c4=c[3],v4=v[3], t4=players[3], c5=c[4],v5=v[4],t5=players[4],c6=c[5],v6=v[5], t6=players[5],c7=c[6],v7=v[6], t7=players[6]
+                           ,c8=c[7],v8=v[7], t8=players[7],c9=c[8],v9=v[8],t9=players[8], c10=c[9],v10=v[9],t10=players[9], c11=c[10],v11=v[10],t11=players[10])
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
