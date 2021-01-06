ebcdc1d6ccf6d4951b30fb7aa0aabf111a12b396
Royston E Tauro <54945757+lucasace@users.noreply.github.com>
2021-01-06 19:07:28 +0530

    [FEATURE] Prepare for first release (#49)
    * [FEATURE] Made the page dynamic
    
    * [LICENSE] Added License doc strings as suggested by AGPLv3 License
    
    * predicts score
    
    * crawler complete
    
    * Website and crawler integrated
    
    * SEO tags
    
    * Cleaned query params(#57)
    
    * Dockerfiles set up(#59)
    
    * Improved UI
    
    Co-authored-by: Shreyas Raviprasad <shreyasraviprasad457@gmail.com>
    Co-authored-by: Sammith S Bharadwaj <54738235+SammithSB@users.noreply.github.com>
    Co-authored-by: scientes <34819304+scientes@users.noreply.github.com>
    Co-authored-by: Bastian <scientes12@gmail.com>
    Co-authored-by: Daniel <54536136+DAN-329@users.noreply.github.com>
    Co-authored-by: SammithSB <sammithsb@gmail.com>

---
+++ app/fantasy_cricket/__init__.py
@@ -0,0 +1,20 @@
+"""
+Module to set path of files in the directory
+Copyright (C) 2020  Royston E Tauro & Sammith S Bharadwaj & Shreyas Raviprasad
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import sys
+
+sys.path.insert(1, "fantasy_cricket/")
--- fantasy_cricket/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Module to set path of files in the directory
-"""
-import sys
-
-sys.path.insert(1, "fantasy_cricket/")
